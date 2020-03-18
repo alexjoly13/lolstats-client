443c51299cf512111c338623aa9504b3926daa8b
Alexandre Joly <alexandre.joly13@gmail.com>
2020-03-18 13:59:14 +0100

    loading screen while request in progress, added npm packages (spinners)

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Redirect, Switch, Route } from "react-router-dom";
+import { Roller } from "react-awesome-spinners";
 import { postSummonerName } from "../api";
 import SummonerResume from "./summonerResume";
 import "./searchBar.css";
@@ -11,6 +12,7 @@ class SearchBar extends Component {
     this.state = {
       summonerName: "",
       summonerInfos: {},
+      isLoading: false,
       isSubmitSuccessful: false
     };
   }
@@ -24,6 +26,7 @@ class SearchBar extends Component {
     event.preventDefault();
     const summSearch = this.state.summonerName;
     let objectReturned = this.state.summonerInfos;
+    this.setState({ isLoading: true });
 
     postSummonerName(summSearch).then(response => {
       console.log("Summoner name search : ", response.data);
@@ -31,36 +34,63 @@ class SearchBar extends Component {
       this.setState({
         summonerName: "",
         isSubmitSuccessful: true,
+        isLoading: false,
         summonerInfos: objectReturned
       });
     });
   }
 
   render() {
-    return this.state.isSubmitSuccessful ? (
-      <div>
-        {/* <Redirect
-          to={{
-            pathname: "/summoner/" + this.state.summonerInfos.name,
-            state: this.state.summonerInfos
-          }}
-        /> */}
-        <SummonerResume summsInfo={this.state.summonerInfos} />
-      </div>
-    ) : (
-      <Form onSubmit={event => this.handleSubmit(event)}>
-        <Form.Control
-          className="summ-bar"
-          size="lg"
-          type="text"
-          placeholder="Summoner Name"
-          id="Searchbar"
-          name="summonerName"
-          value={this.state.summonerName}
-          onChange={event => this.genericOnChange(event)}
-        />
-      </Form>
-    );
+    if (this.state.isSubmitSuccessful) {
+      return <SummonerResume summsInfo={this.state.summonerInfos} />;
+    } else if (this.state.isLoading) {
+      return <Roller />;
+    } else {
+      return (
+        <div>
+          <Form onSubmit={event => this.handleSubmit(event)}>
+            <Form.Control
+              className="summ-bar"
+              size="lg"
+              type="text"
+              placeholder="Summoner Name"
+              id="Searchbar"
+              name="summonerName"
+              value={this.state.summonerName}
+              onChange={event => this.genericOnChange(event)}
+            />
+          </Form>
+        </div>
+      );
+    }
+
+    // return this.state.isSubmitSuccessful ? (
+    //   <div>
+    //     {/* <Redirect
+    //       to={{
+    //         pathname: "/summoner/" + this.state.summonerInfos.name,
+    //         state: this.state.summonerInfos
+    //       }}
+    //     /> */}
+    //     <SummonerResume summsInfo={this.state.summonerInfos} />
+    //   </div>
+    // ) : (
+    //   <div>
+    //     <Form onSubmit={event => this.handleSubmit(event)}>
+    //       <Form.Control
+    //         className="summ-bar"
+    //         size="lg"
+    //         type="text"
+    //         placeholder="Summoner Name"
+    //         id="Searchbar"
+    //         name="summonerName"
+    //         value={this.state.summonerName}
+    //         onChange={event => this.genericOnChange(event)}
+    //       />
+    //     </Form>
+    //     <Roller />
+    //   </div>
+    // );
   }
 }
 
