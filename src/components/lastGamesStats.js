f39fbb583dfb894f568d3a2e8e8251f11d7b9905
Alexandre Joly <alexandre.joly13@gmail.com>
2020-04-27 16:59:43 +0200

    new big function to aggregate champions played over last 10 games + their respective stats

--- src/components/lastGamesStats.js
+++ src/components/lastGamesStats.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import DoughnutChart from "./doughnutChart";
 import { kdaCalculator } from "../helpers/stats-helper";
+import { champImg } from "../helpers/images-helper";
 
 class LastGamesStatistics extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class LastGamesStatistics extends Component {
         totalGames: this.props.gamesArray.length,
         winrate: 0,
       },
+      champsPlayed: [],
       statsLoaded: false,
     };
   }
@@ -46,6 +48,42 @@ class LastGamesStatistics extends Component {
     );
   }
 
+  getChampionsCount(lastGamesArray, searchedSummonerName) {
+    let playerGames = [];
+    lastGamesArray.map((oneGame) => {
+      oneGame.participants.map((oneParticipant) => {
+        return oneParticipant.summonerName === searchedSummonerName
+          ? playerGames.push(oneParticipant)
+          : false;
+      });
+    });
+    playerGames = Object.values(
+      playerGames.reduce((r, { championId, championPlayedName, stats }) => {
+        r[championId] = r[championId] || {
+          championId,
+          championPlayedName,
+          timesPlayed: 0,
+          wins: 0,
+          defeats: 0,
+          kills: 0,
+          deaths: 0,
+          assists: 0,
+        };
+        r[championId].timesPlayed++;
+        r[championId].kills += stats.kills;
+        r[championId].deaths += stats.deaths;
+        r[championId].assists += stats.assists;
+        stats.win === true ? r[championId].wins++ : r[championId].defeats++;
+        return r;
+      }, {})
+    );
+    const sorted = playerGames.sort((a, b) => b.timesPlayed - a.timesPlayed);
+
+    sorted.length > 3
+      ? this.setState({ champsPlayed: sorted.slice(0, 3) })
+      : this.setState({ champsPlayed: sorted });
+  }
+
   getWinLossRatio(lastGamesArray) {
     let w = 0;
     let d = 0;
@@ -68,30 +106,55 @@ class LastGamesStatistics extends Component {
 
   componentDidMount() {
     const lastGames = this.state.lastGames;
-    const name = this.state.summonerName;
+    const leNom = this.state.summonerName;
     this.getWinLossRatio(lastGames);
+    this.getChampionsCount(lastGames, leNom);
   }
 
   render() {
     const playerName = this.state.summonerName;
     const games = this.state.lastGames;
     const stats = this.state.stats;
+    const championsPool = this.state.champsPlayed;
+
+    console.log("Le State :", this.state);
 
     return this.state.statsLoaded ? (
       <section className="last-games-stats">
         <div>
           <div className="container">
             <div className="row">
-              <div className="col-6">
+              <div className="col-4">
                 <div className="d-flex">
                   <span className="mr-1">{stats.wins}W</span>
                   <span className="mr-1">{stats.defeats}D</span>
                   <span>{stats.totalGames} Games</span>
                 </div>
                 <DoughnutChart statsInfo={stats} />
-              </div>
-              <div className="col-6">
                 <div>{this.getAverageKDA(games, playerName)}</div>
+              </div>
+              <div className="col-4">
+                <p>Hello Friend</p>
+              </div>
+              <div className="col-4">
+                <div>
+                  {championsPool.map((oneChamp) => {
+                    return (
+                      <div className="row">
+                        <img
+                          className="inGame-champs-img"
+                          src={champImg(oneChamp.championId)}
+                          alt="played-champ-icon"
+                        />
+                        <p>{oneChamp.championPlayedName}</p>
+                        <p>
+                          ({oneChamp.wins}V {oneChamp.defeats}L)
+                        </p>
+                      </div>
+                    );
+                  })}
+                  <span></span>
+                </div>
               </div>
             </div>
           </div>
